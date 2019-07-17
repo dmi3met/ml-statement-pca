b6fcc59521b26f5b316403fee9cb218fb70d3319
Dmitrii Metelkov <ioniffer@mail.ru>
2019-07-17 17:56:11 +0400

    initial

--- pca.py
+++ pca.py
@@ -23,18 +23,21 @@ import pandas as pd
 часть до двух знаков.
 '''
 # 1 - OK
-data = pd.read_csv('close_prices.csv', index_col=0)
+data_csv = pd.read_csv('close_prices.csv', index_col=0)
 for comp in range(10, 0, -1):
     pca = PCA(n_components=comp)
-    pca.fit(data)
+    pca.fit(data_csv)
     #print(pca.explained_variance_ratio_.sum())
     #print(pca.n_components)
 
 pca.n_components = 4
-data = pca.fit_transform(data)
+data = pca.fit_transform(data_csv)
 # 2 - OK
 dj = pd.read_csv('djia_index.csv', index_col=0)
 corr = np.corrcoef(data[:, 0], dj['^DJI'])
 print(corr)
 #3
-print(pca.components_)
+max_comp = np.argmax(pca.components_[0])
+print(data_csv.keys()[max_comp])
+
+
